4d9f99b1b489cd4618de165cb5b1d6eb00741f18
Kayzu02 <98070725+Kayzu02@users.noreply.github.com>
2022-02-09 17:35:29 +0700

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -11,14 +11,14 @@ from rich.table import Table
 from youtubesearchpython import VideosSearch
 
 from config import (LOG_GROUP_ID, LOG_SESSION, STRING1, STRING2, STRING3,
-                    STRING4, STRING5)
-from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
-                   ASSID1, ASSID2, ASSID3, ASSID4, ASSID5, ASSNAME1, ASSNAME2,
-                   ASSNAME3, ASSNAME4, ASSNAME5, BOT_ID, BOT_NAME, LOG_CLIENT,
+                    STRING4, STRING5 ,STRING6)
+from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5, ASS_CLI_6,
+                   ASSID1, ASSID2, ASSID3, ASSID4, ASSID5, ASSID6, ASSNAME1, ASSNAME2,
+                   ASSNAME3, ASSNAME4, ASSNAME5,ASSNAME6, BOT_ID, BOT_NAME, LOG_CLIENT,
                    OWNER_ID, app)
 from Yukki.Core.Clients.cli import LOG_CLIENT
 from Yukki.Core.PyTgCalls.Yukki import (pytgcalls1, pytgcalls2, pytgcalls3,
-                                        pytgcalls4, pytgcalls5)
+                                        pytgcalls4, pytgcalls5, pytgcalls6)
 from Yukki.Database import (get_active_chats, get_active_video_chats,
                             get_sudoers, is_on_off, remove_active_chat,
                             remove_active_video_chat)
@@ -202,6 +202,25 @@ async def initiate_bot():
             pass
         console.print(f"├[red] Assistant 5 Started as {ASSNAME5}!")
         console.print(f"├[green] ID :- {ASSID5}!")
+    if STRING6 != "None":
+        try:
+            await ASS_CLI_6.send_message(
+                LOG_GROUP_ID,
+                "<b>Congrats!! Assistant Client 6 has started successfully!</b>",
+            )
+        except Exception as e:
+            print(
+                "\nAssistant Account 6 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
+            )
+            console.print(f"\n[red]Stopping Bot")
+            return
+        try:
+            await ASS_CLI_6.join_chat("kayzuchannel")
+            await ASS_CLI_6.join_chat("KayzuSupport")
+        except:
+            pass
+        console.print(f"├[red] Assistant 6 Started as {ASSNAME6}!")
+        console.print(f"├[green] ID :- {ASSID6}!")
     if LOG_SESSION != "None":
         try:
             await LOG_CLIENT.send_message(
@@ -230,6 +249,8 @@ async def initiate_bot():
         await pytgcalls4.start()
     if STRING5 != "None":
         await pytgcalls5.start()
+    if STRING6 != "None":
+        await pytgcalls6.start()
     await idle()
     console.print(f"\n[red]Stopping Bot")
 
